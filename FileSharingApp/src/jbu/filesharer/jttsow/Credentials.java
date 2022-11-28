3a0002ee3ba4f4855be4249c0796fbaad858c349
Salome <salome.oumarou@gmail.com>
2022-11-27 20:16:22 -0600

    credentials ui update

--- FileSharingApp/src/jbu/filesharer/jttsow/Credentials.java
+++ FileSharingApp/src/jbu/filesharer/jttsow/Credentials.java
@@ -1,7 +1,19 @@
 package jbu.filesharer.jttsow;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.io.*;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
 public class Credentials {
 	final static String credentialsDB = "M:\\FA2022\\Java\\Project\\ServerSideDatabase\\credentials.txt";
@@ -61,4 +73,101 @@ public class Credentials {
 			return false;
 		}
 	}
+}
+
+class LogInScreen extends JFrame implements ActionListener{
+	private JButton logInButton;
+	private JButton signUpButton;
+	private JLabel usernameLabel;
+	private JLabel passwordLabel;
+	private JTextField usernameField;
+	private JPasswordField  passwordField;
+	
+	public LogInScreen() {
+		GridBagConstraints layoutConst = null;
+		
+		// set frame's title
+		setTitle("FileSharer: Log-in");
+		
+		// Create labels
+		usernameLabel = new JLabel("Username:");
+		passwordLabel = new JLabel("Password:");
+		
+		// Create Buttons and adding action listeners
+		logInButton = new JButton("log in");
+		logInButton.addActionListener(this);
+		signUpButton = new JButton("sign up");
+		signUpButton.addActionListener(this);
+		
+		// create username field
+		usernameField = new JTextField(15);
+		usernameField.setEditable(true);
+		
+		// create password field
+		passwordField = new JPasswordField(15);
+		passwordField.setEditable(true);
+		
+		// Use a GridBagLayout
+		setLayout(new GridBagLayout());
+		
+		//Specifying components' locations
+		layoutConst = new GridBagConstraints();
+		layoutConst.insets = new Insets(10, 10, 10, 1);
+		layoutConst.gridx = 0;
+		layoutConst.gridy = 0;
+		add(usernameLabel, layoutConst);
+		
+		layoutConst = new GridBagConstraints();
+		layoutConst.insets = new Insets(10, 1, 10, 10);
+		layoutConst.gridx = 1;
+		layoutConst.gridy = 0;
+		add(usernameField, layoutConst);
+		
+		layoutConst = new GridBagConstraints();
+		layoutConst.insets = new Insets(10, 10, 10, 1);
+		layoutConst.gridx = 0;
+		layoutConst.gridy = 1;
+		add(passwordLabel, layoutConst);
+		
+		layoutConst = new GridBagConstraints();
+		layoutConst.insets = new Insets(10, 1, 10, 10);
+		layoutConst.gridx = 1;
+		layoutConst.gridy = 1;
+		add(passwordField, layoutConst);
+		
+		layoutConst = new GridBagConstraints();
+		layoutConst.insets = new Insets(10, 5, 10, 10);
+		layoutConst.gridx = 0;
+		layoutConst.gridy = 2;
+		add(logInButton, layoutConst);
+		
+		layoutConst = new GridBagConstraints();
+		layoutConst.insets = new Insets(10, 5, 10, 10);
+		layoutConst.gridx = 1;
+		layoutConst.gridy = 2;
+		add(signUpButton, layoutConst);
+		
+		System.out.println("test");
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		// TODO Auto-generated method stub
+		String username;
+		String password;
+		
+		username = usernameField.getName();
+		password = passwordField.getName();
+	}
+	
+	public static void main(String[] args) {
+		// Creates logInScreen and its components		
+		LogInScreen myFrame = new LogInScreen();
+		
+		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		myFrame.pack();
+		myFrame.setVisible(true);
+		
+	}
+	
 }
